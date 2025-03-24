3433d1078f457c00b2281ab60b868456b923a818
BeNeuroLIN <paula.pflitsch@lin-magdeburg.de>
2025-03-24 10:26:49 +0100

    initial commit Matlab Code

---
+++ Matlab_SD_analysis/fct_Binary_activity.m
@@ -0,0 +1,29 @@
+function [a_binary_matrix] = fct_Binary_activity(a_clip)
+
+% converts a_clip into a binary activity matrix
+% Sleep is defined as 1 minute or more of no movement. Bins in a_clip have 
+% a length of 1 minute, thus, if a bin contains a
+% number other than 0,the fish were not asleep. In turn, bins with '0'
+% signal that the fish was asleep.
+% By converting the matrix into a binary matrix we can plot sleep vs
+% non-sleep.
+
+a_binary = a_clip>0;
+
+%plot the activity in a heatmap
+%make different groups based on genotype data
+% allWells
+% plot each fish individually in heatmap
+figure('Color',[1 1 1]);
+colormap('bone'); % default bone gray
+ax=imagesc(a_binary(:,allWells)');
+% set(ax,'box','off');
+% colorbar('location','southoutside');
+xlabel('time in seconds');
+ylabel('Individual Fish Sleep (non-active seconds/min)');
+title('Sleep "heatmap"');
+fct_suptitle([' Overview Sleep allWells: ' expName]);
+
+a_binary_matrix = a_binary;
+
+end
